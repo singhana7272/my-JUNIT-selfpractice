cc60dc47981390abaf85165f0c56fbfae83ac066
Anamika Singh <singhana7272@gmail.com>
2023-08-19 02:47:42 +0530

    adding junit projects to repo

---
+++ MyFirstDemoProject/src/com/anamika/exception/NegativeNoNotAllowedException.java
@@ -0,0 +1,7 @@
+package com.anamika.exception;
+
+public class NegativeNoNotAllowedException extends RuntimeException {
+	public NegativeNoNotAllowedException(String message) {
+		super(message);
+	}
+}
---
+++ MyFirstDemoProject/src/com/anamika/test/MyDivideTest.java
@@ -0,0 +1,56 @@
+package com.anamika.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.anamika.demo.MyDivide;
+import com.anamika.exception.NegativeNoNotAllowedException;
+
+public class MyDivideTest {
+	
+	private MyDivide myDivide;
+
+	@Before
+	public void setUp(){
+		myDivide=new MyDivide();
+	}
+
+	@After
+	public void tearDown() {
+		myDivide=null;
+	}
+
+	@Test
+	public void t001(){
+		assertEquals(2,myDivide.divide(10, 5));
+	}
+	
+	@Test(expected= NegativeNoNotAllowedException.class)
+	public void t002(){
+		myDivide.divide(-10, 5);
+	}
+	
+	@Test(expected= NegativeNoNotAllowedException.class)
+	public void t003(){
+		myDivide.divide(10, -5);
+	}
+	
+	@Test(expected=NegativeNoNotAllowedException.class)
+	public void t004(){
+		myDivide.divide(-7, -2);
+	}
+	
+	@Test(expected=ArithmeticException.class)
+	public void t005(){
+		myDivide.divide(20, 0);
+	}
+	
+	@Test
+	public void t006(){
+		 assertEquals(0,myDivide.divide(0, 20));
+	}
+
+}
